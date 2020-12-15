1a1354b806c6afdab3f2270dbc86e90d1d40b96d
Jeremy <jbvidal101@gmail.com>
2020-12-15 15:20:07 -0500

    added the infinityFree database login credentials to the db_conn file

--- includes/db_conn.php
+++ includes/db_conn.php
@@ -1,20 +1,21 @@
 <?PHP
-$username = 'root';
-$password = 'Msql4jbv191';
-$host = 'mysql:dbname=mymoney;host=127.0.0.1';
+#connect to the local database
+// $username = 'root';
+// $password = 'Msql4jbv191';
+// $host = 'mysql:dbname=mymoney;host=127.0.0.1';
+// $conn = new PDO($host, $username, $password);
+// if (!$conn) {
+//   die('Could not connect!');
+// }
 
-#connect to the database or die
-$conn = new PDO($host, $username, $password);
+#connect to the infinityFree database
+$username = 'epiz_26771060';
+$password = 'D2UxU2fUQlgiOPz';
+$host = 'sql202.epizy.com';
+$dbname = 'epiz_26771060_money_tracker_db';
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 if (!$conn) {
   die('Could not connect!');
 }
-// else{
-//     echo "Connection Successful!" . "\n";
-// }
-
-
-
-
-
 
 ?>
